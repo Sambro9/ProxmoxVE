ee484758d4474e8f5fdddc3ed7615f87041cfecc
Sambro9 <84945185+Sambro9@users.noreply.github.com>
2025-08-27 18:50:42 -0500

    Update netdata.sh

--- tools/addon/netdata.sh
+++ tools/addon/netdata.sh
@@ -61,16 +61,6 @@ pve_check() {
     return 0
   fi
 
-  # Proxmox VE 9.x: allow only 9.0
-  if [[ "$PVE_VER" =~ ^9\.([0-9]+)$ ]]; then
-    local MINOR="${BASH_REMATCH[1]}"
-    if ((MINOR != 0)); then
-      msg_error "Unsupported Proxmox VE version: $PVE_VER"
-      msg_error "Supported versions: 8.0 – 8.9 or 9.0"
-      exit 1
-    fi
-    return 0
-  fi
 
   msg_error "Unsupported Proxmox VE version: $PVE_VER"
   msg_error "Supported versions: 8.0 – 8.9 or 9.0"
